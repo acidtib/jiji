a9c25f33a1d6ab0bc2c0b20bf8f0d87359b1549f
acidtib <acidtib@proton.me>
2025-12-11 19:28:14 -0700

    Add help command and default action to main CLI

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,21 @@
 import { Command } from "@cliffy/command";
+import { HelpCommand } from "@cliffy/command/help";
 import { initCommand } from "./commands/init.ts";
 import { serverCommand } from "./commands/server/index.ts";
 import { auditCommand } from "./commands/audit.ts";
 import { VERSION } from "./version.ts";
 
-await new Command()
+const command = new Command()
   .name("jiji")
   .version(VERSION)
-  .description("Jiji CLI - Infrastructure management tool")
+  .description("Jiji")
+  .action(() => {
+    command.showHelp();
+    Deno.exit(0);
+  })
   .command("init", initCommand)
   .command("server", serverCommand)
   .command("audit", auditCommand)
-  .parse(Deno.args);
+  .command("help", new HelpCommand());
+
+await command.parse(Deno.args);
