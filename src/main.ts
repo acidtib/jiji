b880badb3d6f63217ae1d97037378751b0de4dd0
acidtib <acidtib@proton.me>
2025-12-11 19:40:27 -0700

    Add version command

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,11 @@ import { HelpCommand } from "@cliffy/command/help";
 import { initCommand } from "./commands/init.ts";
 import { serverCommand } from "./commands/server/index.ts";
 import { auditCommand } from "./commands/audit.ts";
-import { VERSION } from "./version.ts";
+import { versionCommand } from "./commands/version.ts";
 
 const command = new Command()
   .name("jiji")
-  .version(VERSION)
-  .description("Jiji")
+  .description("Jiji - Infrastructure management tool")
   .action(() => {
     command.showHelp();
     Deno.exit(0);
@@ -16,6 +15,7 @@ const command = new Command()
   .command("init", initCommand)
   .command("server", serverCommand)
   .command("audit", auditCommand)
+  .command("version", versionCommand)
   .command("help", new HelpCommand());
 
 await command.parse(Deno.args);
