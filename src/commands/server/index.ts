f028b7ea09d5aae6cd5773a9f4bfce3420630efe
acidtib <acidtib@proton.me>
2025-12-21 18:18:38 -0700

    feat(server): add teardown command to remove servers and network

--- src/commands/server/index.ts
+++ src/commands/server/index.ts
@@ -1,8 +1,10 @@
 import { Command } from "@cliffy/command";
 import { initCommand } from "./init.ts";
 import { execCommand } from "./exec.ts";
+import { teardownCommand } from "./teardown.ts";
 
 export const serverCommand = new Command()
   .description("Server management commands")
   .command("init", initCommand)
-  .command("exec", execCommand);
+  .command("exec", execCommand)
+  .command("teardown", teardownCommand);
