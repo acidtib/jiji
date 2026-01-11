c082ee0618cbfe374b3c43767cdd184e87e937f0
acidtib <acidtib@proton.me>
2026-01-10 23:26:08 -0700

    chore(dns): bump jiji-dns version to 0.3.3 to pick up latest fixes

--- src/lib/network/dns.ts
+++ src/lib/network/dns.ts
@@ -13,7 +13,7 @@ const JIJI_DNS_INSTALL_DIR = "/opt/jiji/dns";
 
 // jiji-dns GitHub releases URL
 const JIJI_DNS_REPO = "acidtib/jiji-dns";
-const JIJI_DNS_VERSION = "0.3.2";
+const JIJI_DNS_VERSION = "0.3.3";
 
 /**
  * Install jiji-dns on a remote server
